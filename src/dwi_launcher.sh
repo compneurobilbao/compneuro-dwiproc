326c0f04cc4ef951ec231bab6b58cd781bac4a8e
ajimenezmarin <antonio93jm@gmail.com>
2024-10-25 12:44:54 +0200

    ENH: skipping dwipreproc if it is already done

--- src/dwi_launcher.sh
+++ src/dwi_launcher.sh
@@ -11,20 +11,24 @@ timestamp_initial=$(date +"%H:%M")
 mkdir -p /project/log
 touch /project/log/Dwipreproc_${timestamp_initial}.txt
 partition=$1
-
+partition_name=$(basename $partition .nii.gz)
 while read line
 do
     participant=$( echo ${line} | awk '{ print $1 }')
 
-	if [  -f "/project/Preproc/Dwiprep/${participant}_preprocessed.nii.gz" ]; then
+	if [  -f "/project/Preproc/Dwiprep/${participant}/${partition_name}_det.csv" ]; then
         echo "$participant already processed" >> /project/log/Dwipreproc_${timestamp_initial}.txt
     else
         echo "*********************" >> /project/log/Dwipreproc_${timestamp_initial}.txt
         echo "$participant" >> /project/log/Dwipreproc_${timestamp_initial}.txt
         echo "*********************" >> /project/log/Dwipreproc_${timestamp_initial}.txt
 
-        source /app/src/dwi_preproc.sh $participant $timestamp_initial
-        source /app/src/dwi_tract.sh $participant $timestamp_initial $partition
+        if [ -f "/project/Preproc/Dwiprep/${participant}/dwi_clean.nii.gz" ]; then
+            echo "Dwi data already cleaned" >> /project/log/Dwipreproc_${timestamp_initial}.txt
+        else
+            source /app/src/dwi_preproc.sh $participant $timestamp_initial
+        fi
+        source /app/src/dwi_tract.sh $participant $timestamp_initial $partition_name
    fi
 	
 done < <(tail -n +2 /project/data/participants.tsv)
